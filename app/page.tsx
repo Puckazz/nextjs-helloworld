bea5eba4e332a155a520ed6731c77091fc671164
Luong Bao Phuc <ppbao1912@gmail.com>
2025-04-03 14:15:13 +0700

    add css

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
 export default function Home() {
   return (
-    <div>
-      Lương Bảo Phúc xin chào NextJS
+    <div className="flex justify-center items-center w-full h-screen">
+      <p className="font-bold text-2xl hover:text-blue-400 transition-colors duration-500 ease-in-out">
+        Lương Bảo Phúc Xin chào NextJS
+      </p>
     </div>
   );
 }
